117d06d3daae6d76b16e1175a3b9d47e28ec4407
Lyken17 <ligeng@mit.edu>
2021-01-17 01:55:14 +0800

    add webseeders

--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
 import os, os.path as osp
+from pprint import pprint
+from copy import deepcopy
 
 # popular trackers
 extra_trackers = '''
@@ -13,33 +15,33 @@ extra_trackers += '''
 tr=wss%3A%2F%2Ftracker.openwebtorrent.com
 '''
 
-# Lyken's self-hosted tracker as backup
+# Lyken's self-hosted tracker (Japan) as backup
 extra_trackers += '''
-tr=http%3A%2F%2Ftr.syu.life%3A8000%2Fannounce
-tr=udp%3A%2F%2Ftr.syu.life%3A8000
-tr=ws%3A%2F%2Ftr.syu.life%3A8000
+tr=http%3A%2F%2Ffrp-jp.lzhu.me%3A8000%2Fannounce
+tr=udp%3A%2F%2Ffrp-jp.lzhu.me%3A8000
+tr=ws%3A%2F%2Ffrp-jp.lzhu.me%3A8000
 '''
 
 # Lyken's self-hosted tracker (China) as backup
-extra_trackers += '''
-tr=http%3A%2F%2Fcn.tr.syu.life%3A8080%2Fannounce
-tr=udp%3A%2F%2Fcn.tr.syu.life%3A8080
-tr=ws%3A%2F%2Fcn.tr.syu.life%3A8080
-'''
+# extra_trackers += '''
+# tr=http%3A%2F%2Fcn.tr.syu.life%3A8080%2Fannounce
+# tr=udp%3A%2F%2Fcn.tr.syu.life%3A8080
+# tr=ws%3A%2F%2Fcn.tr.syu.life%3A8080
+# '''
 
 extra_trackers = [_ for _ in extra_trackers.strip().split("\n") if len(_) > 2]
 
-extra_trackers = "&".join(extra_trackers)
+# extra_trackers = "&".join(extra_trackers)
+# print(extra_trackers)
 
-print(extra_trackers)
-
-def load_yaml( filename ):
-	with open( filename, 'rb' ) as f:
+def load_yaml(filename):
+	with open(filename, 'rb' ) as f:
 		return yaml.load( f, Loader=yaml.FullLoader)
 
-def generate_bangumi(curr_data, prev_data=None, next_data=None, extra_trackers=extra_trackers, dirname="docs"):
+def generate_bangumi(curr_data, meta, prev_data=None, next_data=None, extra_trackers=extra_trackers, dirname="docs", ):
 	env = Environment(
-		loader=FileSystemLoader(os.path.dirname(os.path.abspath(__file__))),trim_blocks=True
+		loader=FileSystemLoader(os.path.dirname(os.path.abspath(__file__))),
+		trim_blocks=True
 	)
 	template = env.get_template('template_bangumi.html')
 	'''
@@ -50,15 +52,27 @@ def generate_bangumi(curr_data, prev_data=None, next_data=None, extra_trackers=e
 	'''
 	# prev_id, curr_id, next_id = ("ep-%d.html" % (index + i) for i in (-1, 0, 1))
 	curr_id = curr_data["episode"]
-	bangumi = {
-		"name" : "Shingeki no Kyojin",
-	}
+	
+	temp_trackers = deepcopy(extra_trackers)
+	# global meta
+	if 'webseeds' in meta:
+		episode = curr_data["episode"]
+		webseeds = meta['webseeds']
+		if 'lambda' in webseeds:
+			episode = eval(webseeds['lambda'])
+		# webseeds['torrent'].format(episode=episode)
+		if 'torrent' in webseeds:
+			temp_trackers.append("xs=%s" % webseeds['torrent'].format(episode=episode))
+		if 'video' in webseeds:
+			temp_trackers.append("ws=%s" % webseeds['video'].format(episode=episode))
+	pprint(temp_trackers)
+	# exit(0)
 	htmlpage = template.render(
 			prev=prev_data, 
 			curr=curr_data, 
 			next=next_data, 
-			extra_trackers="&" + extra_trackers,
-			bangumi=bangumi
+			extra_trackers="&" + "&".join(temp_trackers),
+			meta=meta,
 		)
 	
 	# filename = "docs/ep-%s.html" % curr_id
@@ -69,12 +83,14 @@ def generate_bangumi(curr_data, prev_data=None, next_data=None, extra_trackers=e
 
 def generate_index(bangumi, dirname="docs"):
 	env = Environment(
-		loader=FileSystemLoader(os.path.dirname(os.path.abspath(__file__)) ),
-		trim_blocks=True )
+		loader=FileSystemLoader(os.path.dirname(os.path.abspath(__file__))),
+		trim_blocks=True
+	)
 	template = env.get_template('template_index.html')
 
 	htmlpage = template.render(
-			bangumi=bangumi
+			tumi=yinfo['tumi'],
+			meta=yinfo['meta']
 		)
 	
 	# filename = "docs/index.html"
@@ -82,17 +98,30 @@ def generate_index(bangumi, dirname="docs"):
 	with open(filename, 'w') as f:
 		f.write(htmlpage)
 		print("Successfuly generate_index %s" % filename)
-	
-data = load_yaml( "bangumi/shingeki.yaml" )
+
+bangumi = "Shingeki no Kyojin"	
+# bangumi = "Kimetsu no Yaiba"	
+yinfo = load_yaml(f"bangumi/{bangumi}.yaml" )
+
+data = yinfo["tumi"]
 items = len(data)
-dirname = "docs/Shingeki no Kyojin"
+
+dirname = f"docs/{bangumi}"
 os.makedirs(dirname, exist_ok=True)
 generate_index(data, dirname=dirname)
+
+
+# global meta
+meta = yinfo['meta']
+# episode = 1
+# webseeds = yinfo['meta']['webseeds']
+# episode = eval(webseeds['lambda'])
+# print(webseeds['torrent'].format(episode=episode))
 
 with open("all_maglinks.txt", "w") as fp:
 	for i in range(items):
 		prev_data = data[i-1] if i - 1 >= 0 else None
 		next_data = data[i+1] if i + 1 < items else None
 		curr_data = data[i]
-		generate_bangumi(curr_data, prev_data, next_data, dirname=dirname)
-		fp.write(curr_data["magnetlink"] + "&" + extra_trackers + "\n")
+		generate_bangumi(curr_data, meta, prev_data, next_data, dirname=dirname)
+		# fp.write(curr_data["magnetlink"] + "&" + extra_trackers + "\n")
